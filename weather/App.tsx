4ab86c58b35f28c75847e0782ee512c6f71dd1f4
pkiop <pkiopb@gmail.com>
2021-12-20 22:23:44 +0900

    Put dummy content

--- weather/App.tsx
+++ weather/App.tsx
@@ -4,32 +4,48 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.v1}></View>
-      <View style={styles.v2}></View>
-      <View style={styles.v3}></View>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
+      <StatusBar style='light' />
     </View>
   );
 }
 
-const size = {
-  flex: 1,
-};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-  },
-  v1: {
-    ...size,
     backgroundColor: 'tomato',
   },
-  v2: {
-    ...size,
-    flex: 2,
-    backgroundColor: 'teal',
+  city: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  v3: {
-    ...size,
-    backgroundColor: 'orange',
+  cityName: {
+    fontSize: 68,
+    fontWeight: '500',
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
   },
 });
